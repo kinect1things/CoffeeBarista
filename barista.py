f1c6b36d850bbd8bfddfdd9e17f610b12b6fa2c3
kinect1things <connect_online@hotmail.com>
2021-09-21 20:19:27 -0500

    initial commit

---
+++ barista.py
@@ -0,0 +1,18 @@
+### I'm going to build a Coffee Barista
+### Shout Out to Network Chuck
+### https://www.youtube.com/watch?v=IXr0-J5XXMA
+
+
+
+print("Hello, Welcome to Coffee Barista. Thank you so much for coming in today!")
+
+u_name = input("What's your name?\n")
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+print("Hello "+u_name+"!"+" What can I get you today? Here's our menu..\n")
+
+print(menu+"\n")
+
+order = input()
+
+print("Sounds Good "+u_name+"."+" We'll have that "+order+" right out!")
